c419372ce2685bce0e1a4a0819c05ba8e893555f
faseel <faseelm98@gmail.com>
2021-01-06 20:52:09 +0530

    Completed Search Function

--- lib/screens/analytics/analyticsdata.dart
+++ lib/screens/analytics/analyticsdata.dart
@@ -1,9 +1,7 @@
 /* services/firestore_service.dart */
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:imd/models/imd.dart';
-import 'package:imd/models/analytics.dart';
 import 'package:imd/global.dart' as global;
 
 class AnalyticServices {
@@ -27,10 +25,20 @@ class AnalyticServices {
       }).toList();
     }
 
+    if (global.area == '') {
+      global.area = null;
+    }
+    if (global.group == '') {
+      global.group = null;
+    }
+    if (global.activity == '') {
+      global.activity = null;
+    }
+
     return databaseReference
         .collection("imd") // I need the variable from the dropdown menu
         .where('area', isEqualTo: global.area)
-        .where('group', isEqualTo: global.group)
+        .where('grouo', isEqualTo: global.group)
         .where('activity', isEqualTo: global.activity)
         .orderBy('date', descending: true)
         .snapshots()
